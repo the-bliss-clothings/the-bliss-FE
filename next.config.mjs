e4d44f9add233e2c41b31645cbe6f8f62e1ee638
YohanNayanajith <yohannayanajith40@gmail.com>
2024-05-25 02:13:21 +0530

    task/change next config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  images: {
+    domains: ["res.cloudinary.com"],
+  },
+  reactStrictMode: true,
+  swcMinify: true,
+};
 
 export default nextConfig;
