b2b500d58e2b5e91dd3fcc82dee83189b9707bfc
YohanNayanajith <yohannayanajith40@gmail.com>
2024-05-26 12:41:22 +0530

    task/add initial changes

---
+++ src/app/(root)/(auth)/signup/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>Sign Up page</div>
+  )
+}
+
+export default page
---
+++ src/app/(root)/(home)/page.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <main>
+      <section>page</section>
+    </main>
+  );
+};
+
+export default page;
